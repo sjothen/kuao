e2ae227b6bd99b0a77e6ae9917f3ade71ffdd9cc
Stephen Jothen <sjothen@gmail.com>
2013-01-18 12:01:45 +0200

    Add basic eval and toplevel definitions

--- kuao.py
+++ kuao.py
@@ -18,6 +18,15 @@ class Symbol:
     else:
       return '%d' % (self.tag,)
 
+def listp(e):
+  return isinstance(e, list)
+def symbolp(e):
+  return e.tag == Symbol.SYMBOL
+def stringp(e):
+  return e.tag == Symbol.STRING
+def numberp(e):
+  return e.tag == Symbol.NUMBER
+
 class Reader:
   def __init__(self, strm):
     self.stream = strm
@@ -125,29 +134,117 @@ class Parser:
       else:
         l.append(tok)
 
-def main():
-  par = Parser(Lexer())
-  while True:
-    # Can't use print with ,: it forces leading space next print
-    sys.stdout.write('> ')
-    sexp = par.read()
-    if sexp is None:
-      break
-    print sexp
-
-if __name__ == '__main__':
-  main()
-
 class Env:
   def __init__(self, parent=None):
     self.parent = parent
     self.bindings = {}
-  def get(self, key):
+  def lookup(self, key):
     if self.bindings.has_key(key):
       return self.bindings[key]
     elif self.parent:
-      return self.parent.get(key)
+      return self.parent.lookup(key)
     else:
       raise KeyError, key
-  def set(self, key, value):
+  def add(self, key, value):
     self.bindings[key] = value
+  def merge(self, d):
+    for k in d.keys():
+      self.bindings[k] = d[k]
+
+class KuaoException(Exception):
+  pass
+
+class Closure:
+  def __init__(self, env, args, body):
+    self.env = env
+    self.args = args
+    self.body = body
+
+def closurep(exp):
+  return isinstance(exp, Closure)
+
+toplevel = Env()
+
+def kuaoeval(env, exp):
+  if listp(exp):
+    if len(exp) == 0:
+      return exp
+    else:
+      car, cdr = kuaoeval(env, exp[0]), exp[1:]
+      if callable(car):
+        return car(env, cdr)
+      elif closurep(car):
+        return car.call(cdr)
+      else:
+        raise KuaoException, "invalid function application"
+  elif symbolp(exp):
+    return env.lookup(exp.value)
+  elif stringp(exp) or numberp(exp):
+    return exp.value
+
+def define(env, exp):
+  if len(exp) != 2:
+    raise KuaoException, "define requires 2 arguments"
+  bnd = exp[0]
+  exp = exp[1]
+  if symbolp(bnd):
+    e = kuaoeval(env, exp)
+    env.add(bnd.value, e)
+    return e
+  #return Closure(env, code)
+
+def mkclosure(env, exp):
+  pass
+
+def plus(env, exp):
+  total = 0
+  for e in exp:
+    total = op(total, kuaoeval(env, e))
+  return total
+
+def mkop1(fn):
+  def op(env, exp):
+    if len(exp) == 0:
+      raise KuaoException, "needs at least 1 arg"
+    total = kuaoeval(env, exp[0])
+    for e in exp[1:]:
+      total = fn(total, kuaoeval(env, e))
+    return total
+  return op
+
+def mkop(default, fn):
+  def op(env, exp):
+    total = default
+    for e in exp:
+      total = fn(total, kuaoeval(env, e))
+    return total
+  return op
+
+toplevel.merge({
+  'define': define,
+  'lambda': mkclosure,
+  '+': mkop(0, lambda a, b: a+b),
+  '-': mkop1(lambda a, b: a-b),
+  '*': mkop(1, lambda a, b: a*b),
+  '/': mkop1(lambda a, b: a/b)
+})
+
+def main():
+  par = Parser(Lexer())
+  while True:
+    # Can't use print with ,: it forces leading space next print
+    sys.stdout.write('kuao> ')
+    sexp = par.read()
+    if sexp is None:
+      break
+    try:
+      ret = kuaoeval(toplevel, sexp)
+      if listp(ret):
+        print ("(" + " ".join(ret) + ")")
+      else:
+        print ret
+    except KuaoException as e:
+      print e
+
+if __name__ == '__main__':
+  main()
